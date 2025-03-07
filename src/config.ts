19583fecb5d04f2482b0e5aee22ade62a0e02552
alphainfinitus <kumar.mridul000@gmail.com>
2025-03-08 02:02:39 +0530

    Update to Mythos parachain configuration

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
-import {lookupArchive} from "@subsquid/archive-registry"
+import { lookupArchive } from "@subsquid/archive-registry";
 
 const config: any = {
-    chain: {
-        name: 'rolimec',
-        prefix: 41,
-    },
-    typesBundle: 'polkadot',
-    batchSize: 500,
-    blockRange: {
-        from: 0,
-    },
-}
+  chain: {
+    name: "mythos",
+    prefix: 29972,
+  },
+  typesBundle: "polkadot",
+  batchSize: 500,
+  blockRange: {
+    from: 0,
+  },
+};
 
-export default config
+export default config;
