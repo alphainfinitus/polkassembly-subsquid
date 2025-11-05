45729702ae7be2d9ab1fc3d38cdd55a9334c5ca2
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2025-11-05 21:59:56 +0100

    remove conditional

--- src/processor.ts
+++ src/processor.ts
@@ -426,11 +426,7 @@ export async function handleBlocks(ctx: any, config: ChainConfig) {
                     await modules.treasury.events.handleSpendApproved(ctx, item, block.header)
                 }
                 if (item.name == 'Treasury.AssetSpendApproved') {
-                    if (config.name === 'polkadot') {
-                        await polkadotModules.treasury.events.handleAssetSpendApproved(ctx, item, block.header, block)
-                    } else if (config.name === 'assethub-polkadot') {
-                        await modules.treasury.events.handleAssetSpendApproved(ctx, item, block.header, block)
-                    }
+                    await modules.treasury.events.handleAssetSpendApproved(ctx, item, block.header, block)
                 }
 
                 if (config.hasDemocracy && config.name === 'polkadot') {
